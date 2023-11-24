41b45174e97b2730c6a3e02df5b644dbacbb3b79
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-24 19:56:48 +0400

    updates

--- src/components/add-item/AddItem.jsx
+++ src/components/add-item/AddItem.jsx
@@ -3,18 +3,59 @@ import "./add-item.css";
 
 const AddItem = () => {
   return (
-    <div>
-      <div>
-        <p>Հայտարարության տեսակ</p>
-        <div>
-          <input type="radio"/>
+    <div className="add-item">
+      <form className="add-item-panel">
+        <div className="select-item-panel">
+          <h4>Հայտարարության տեսակ</h4>
+          <div className="item-type-panel">
+            <label className="item-type">
+              <span>Վաճառք</span>
+              <input type="radio" name='type'/>
+            </label>
+            <label className="item-type">
+              <span>Վարձակալություն</span>
+              <input type="radio" name='type'/>
+            </label>
+          </div>
         </div>
-        <div>
-          <input type="radio"/>
+        <div className="select-item-panel">
+          <h4>Գույքի տեսակ</h4>
+          <div className="item-type-panel">
+            <label className="item-type">
+              <span>Բնակարան</span>
+              <input type="radio" name='item-type'/>
+            </label>
+            <label className="item-type">
+              <span>Առանձնատուն</span>
+              <input type="radio" name='item-type'/>
+            </label>
+            <label className="item-type">
+              <span>Կոմերցիոն</span>
+              <input type="radio" name='item-type'/>
+            </label>
+            <label className="item-type">
+              <span>Հողատարածք</span>
+              <input type="radio" name='item-type'/>
+            </label>
+          </div>
         </div>
-      </div>
-      <form>
-        <input />
+        <hr />
+        <div className="item-headers">
+          <div className="header-inpts">
+            <input placeholder="Գույքի անվանումը ՝ Հայերեն"/>
+            <textarea placeholder="Գույքի նկարագրությունը ՝ Հայերեն"></textarea>
+          </div>
+          <hr />
+          <div className="header-inpts">
+            <input placeholder="Գույքի անվանումը ՝ Ռուսերեն"/>
+            <textarea placeholder="Գույքի նկարագրությունը ՝ Ռուսերեն"></textarea>
+          </div>
+          <hr />
+          <div className="header-inpts">
+            <input placeholder="Գույքի անվանումը ՝ Անգլերեն"/>
+            <textarea placeholder="Գույքի նկարագրությունը ՝ Անգլերեն"></textarea>
+          </div>
+        </div>
       </form>
     </div>
   );
