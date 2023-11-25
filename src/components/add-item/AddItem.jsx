2567918f5929de0bc2e35a47951b665241ea225c
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-25 18:05:05 +0400

    updates

--- src/components/add-item/AddItem.jsx
+++ src/components/add-item/AddItem.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import "./add-item.css";
 import { FaImage } from "react-icons/fa6";
 import { FaTrashCan } from "react-icons/fa6";
+import { FcPicture } from "react-icons/fc";
+
 
 const AddItem = () => {
   const [mainImg, setMainImg] = useState("");
+  const [anotherImgs, setAnotherImgs] = useState([]);
 
   return (
     <div className="add-item">
@@ -83,13 +86,6 @@ const AddItem = () => {
             <input placeholder="Գույքի մակերեսը" type="number" />
           </div>
         </div>
-        {/* <div>
-          <select>
-            <option>Երևան</option>
-            <option>Կոտայք</option>
-
-          </select>
-        </div> */}
         <input
           type="file"
           id="main-image"
@@ -111,13 +107,56 @@ const AddItem = () => {
                 src={mainImg}
                 height={"200px"}
                 width={"100%"}
-                style={{objectFit: 'cover', borderRadius: '7px'}}
+                style={{ objectFit: "cover", borderRadius: "7px" }}
               />
               <button onClick={() => setMainImg("")}>
                 <FaTrashCan />
               </button>
             </div>
           )}
+        </div>
+        <h4>Այլ նկարներ</h4>
+        <div className="another-images">
+          <input
+            type="file"
+            id="add-image"
+            accept="image/*"
+            multiple
+            onChange={(evn) => {
+              const fileList = evn.target.files;
+              const newImages = Array.from(fileList).map((file) =>
+                URL.createObjectURL(file)
+              );
+              setAnotherImgs((prevImages) => [...prevImages, ...newImages]);
+              console.log(anotherImgs);
+            }}
+          />
+          <div className="add-image">
+            <label htmlFor="add-image">
+              Ավելացնել նկար +
+            </label>
+          </div>
+          <div className="an-images">
+            {anotherImgs.map((image, index) => (
+              <div className="item-image">
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Uploaded ${index + 1}`}
+                  width={"100%"}
+                  height={"250px"}
+                  style={{ objectFit: "cover", borderRadius: "7px" }}
+                />
+                <button
+                  onClick={() => {
+                    setAnotherImgs(anotherImgs.filter((el) => el !== image));
+                  }}
+                >
+                  <FaTrashCan />
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </form>
     </div>
