9e03c3c64372b3aaf275ec2021acc76f904b690f
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-27 12:05:38 +0400

    updates

--- src/components/item-card/ItemCard.jsx
+++ src/components/item-card/ItemCard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./item-card.css";
 import { FaPenToSquare } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaShareNodes } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa6";
+import { FaTrashCan } from "react-icons/fa6";
 
 const ItemCard = () => {
   return (
@@ -13,8 +17,24 @@ const ItemCard = () => {
           className="item-img"
         />
         <b>Նոր Նորք, Գյուրջյան փող</b>
-        <p>Գյուրջյան փող, Նոր Նորք, Երևան</p>
+        <p className="item-location">
+          <FaLocationDot />
+          Գյուրջյան փող, Նոր Նորք, Երևան
+        </p>
         <b>$ 130,000</b>
+      </div>
+      <div className="broker-info">
+        <img
+          height={"150px"}
+          width={"100%"}
+          src="https://www.theforage.com/blog/wp-content/uploads/2022/07/stockbroker.jpg"
+          className="broker-profile-img"
+        />
+        <div className="brokers-all-data">
+          <b>Դավիթ Մանուկյան</b>
+          <span>+37494673735</span>
+          <span>davitmanukyan.d@gmail.com</span>
+        </div>
       </div>
       <div className="item-btns">
         <button className="btn1">
@@ -22,10 +42,18 @@ const ItemCard = () => {
           Փոփոխել
         </button>
         <button className="btn2">Պատմություն</button>
-        <button className="btn3">Կիսվել</button>
-        <button className="btn4">Փոփոխել</button>
-        <button className="btn5">Պատմություն</button>
-        <button className="btn6">Ջնջել</button>
+        <button className="btn3">
+          <FaShareNodes />
+          Կիսվել
+        </button>
+        <button className="btn4">
+          <FaEye />
+          Նայել
+        </button>
+        <button className="btn6">
+          <FaTrashCan />
+          Ջնջել
+        </button>
       </div>
     </div>
   );
