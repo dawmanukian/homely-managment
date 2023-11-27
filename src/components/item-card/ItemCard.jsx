db3640e74d0feb4f19fa2561d143ce31c8054fd1
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-27 21:14:47 +0400

    updates

--- src/components/item-card/ItemCard.jsx
+++ src/components/item-card/ItemCard.jsx
@@ -5,23 +5,29 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaShareNodes } from "react-icons/fa6";
 import { FaEye } from "react-icons/fa6";
 import { FaTrashCan } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa6";
+import { TbMail } from "react-icons/tb";
 
 const ItemCard = () => {
   return (
     <div className="item-card">
-      <div>
+      <div className="item-data-panel">
         <img
           src="https://housing.com/news/wp-content/uploads/2023/03/exterior-design-shutterstock_1932966368-1200x700-compressed.jpg"
           width={"100%"}
           height={"200px"}
           className="item-img"
         />
-        <b>Նոր Նորք, Գյուրջյան փող</b>
+        <b className="item-header">Նոր Նորք, Գյուրջյան փող</b>
         <p className="item-location">
           <FaLocationDot />
           Գյուրջյան փող, Նոր Նորք, Երևան
         </p>
-        <b>$ 130,000</b>
+        <div className="item-prices">
+          <b>$ 130,000</b>
+          <b>֏ 130,000</b>
+          <b>₽ 130,000</b>
+        </div>
       </div>
       <div className="broker-info">
         <img
@@ -32,8 +38,14 @@ const ItemCard = () => {
         />
         <div className="brokers-all-data">
           <b>Դավիթ Մանուկյան</b>
-          <span>+37494673735</span>
-          <span>davitmanukyan.d@gmail.com</span>
+          <span>
+            <FaPhone />
+            +37494673735
+          </span>
+          <span>
+            <TbMail style={{fontSize: '25px'}}/>
+            davitmanukyan.d@gmail.com
+          </span>
         </div>
       </div>
       <div className="item-btns">
